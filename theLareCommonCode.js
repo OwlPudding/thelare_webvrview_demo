a62734b9afb5f6b7fa62396709ebaa5640f60b50
Anthony Cortes-Ose <acortesose@gmail.com>
2017-08-05 15:28:31 -0400

    stupid quote

--- theLareCommonCode.js
+++ theLareCommonCode.js
@@ -1,4 +1,4 @@
 var vrViewPlayer = function(elementIdName, options){
-	console.log("Creating Web VR View Player);
+	console.log("Creating Web VR View Player");
 	return new VRView.Player(elementIdName, options);
 }
