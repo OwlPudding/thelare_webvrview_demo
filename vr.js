a1313019c6b695e83e1e931a0220b9df26f4a2d9
OwlPudding <acortesose@gmail.com>
2017-08-12 16:38:25 -0400

    adding hotspots, will update descriptions, Jesus

--- vr.js
+++ vr.js
@@ -10,10 +10,46 @@ myPicture.on('ready',function(e){
     radius: 0.10,
     distance: 1
   });
+  myPicture.addHotspot('hotspot-2',{
+    pitch: 10,
+    yaw: 20,
+    radius: 0.10,
+    distance: 1
+  });
+  myPicture.addHotspot('hotspot-3',{
+    pitch: 30,
+    yaw: 40,
+    radius: 0.10,
+    distance: 1
+  });
+  myPicture.addHotspot('hotspot-4',{
+    pitch: 40,
+    yaw: 70,
+    radius: 0.10,
+    distance: 1
+  });
+  myPicture.addHotspot('hotspot-5',{
+    pitch: 70,
+    yaw: 90,
+    radius: 0.10,
+    distance: 1
+  });
 });
 myPicture.on('click',function(e){
   console.log("register all clicks");
   if(e.id == "hotspot-1"){
     alert("Clicked HOTSPOT 1");
+  }
+  if(e.id == "hotspot-2"){
+    alert("Clicked HOTSPOT 2");
+  }
+  if(e.id == "hotspot-3"){
+    alert("Clicked HOTSPOT 3");
+  }
+  if(e.id == "hotspot-4"){
+    alert("Clicked HOTSPOT 4");
+  }
+  if(e.id == "hotspot-5"){
+    alert("Clicked HOTSPOT 5");
   }
 });
